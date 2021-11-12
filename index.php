10f42e23f24f9e72872a02a2b034ad6c3aeaa6db
Raul202 <55659437+RiceaRaul@users.noreply.github.com>
2021-11-12 04:14:47 +0200

    LEAFLET GTA V MAP

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+   <head>
+      <title>GTA V MAP</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico">
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="">
+      <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+   </head>
+   <style>
+      #map { height: 100%; }
+   </style>
+   <body>
+      <div id="map"/>
+	     <script src="scripts/script.js" ></script>
+   </body>
+</html>
---
+++ scripts/script.js
@@ -0,0 +1,61 @@
+const center_x = 117.3;
+const center_y = 172.8;
+const scale_x = 0.02072;
+const scale_y = 0.0205;
+
+CUSTOM_CRS = L.extend({}, L.CRS.Simple, {
+    projection: L.Projection.LonLat,
+    scale: function(zoom) {
+
+        return Math.pow(2, zoom);
+    },
+    zoom: function(sc) {
+
+        return Math.log(sc) / 0.6931471805599453;
+    },
+	distance: function(pos1, pos2) {
+        var x_difference = pos2.lng - pos1.lng;
+        var y_difference = pos2.lat - pos1.lat;
+        return Math.sqrt(x_difference * x_difference + y_difference * y_difference);
+    },
+	transformation: new L.Transformation(scale_x, center_x, -scale_y, center_y),
+    infinite: true
+});
+
+var SateliteStyle = L.tileLayer('mapStyles/styleSatelite/{z}/{x}/{y}.jpg', {minZoom: 0,maxZoom: 8,noWrap: true,continuousWorld: false,attribution: 'Online map GTA V',id: 'SateliteStyle map',}),
+	AtlasStyle	= L.tileLayer('mapStyles/styleAtlas/{z}/{x}/{y}.jpg', {minZoom: 0,maxZoom: 5,noWrap: true,continuousWorld: false,attribution: 'Online map GTA V',id: 'styleAtlas map',}),
+	GridStyle	= L.tileLayer('mapStyles/styleGrid/{z}/{x}/{y}.png', {minZoom: 0,maxZoom: 5,noWrap: true,continuousWorld: false,attribution: 'Online map GTA V',id: 'styleGrid map',});
+
+var ExampleGroup = L.layerGroup();
+
+var Icons = {
+	"Example" :ExampleGroup,
+};
+
+var mymap = L.map('map', {
+    crs: CUSTOM_CRS,
+    minZoom: 1,
+    maxZoom: 5,
+    Zoom: 5,
+    maxNativeZoom: 5,
+    preferCanvas: true,
+    layers: [SateliteStyle],
+    center: [0, 0],
+    zoom: 3,
+});
+
+var layersControl = L.control.layers({ "Satelite": SateliteStyle,"Atlas": AtlasStyle,"Grid":GridStyle}, Icons).addTo(mymap);
+
+
+function customIcon(icon){
+	return L.icon({
+		iconUrl: `blips/${icon}.png`,
+		iconSize:     [20, 20],
+		iconAnchor:   [20, 20], 
+		popupAnchor:  [-10, -27]
+	});
+}
+
+var X  = 0;
+var Y = 0;
+L.marker([Y,X], {icon: customIcon(1)}).addTo(Icons["Example"]).bindPopup("I am here.");
